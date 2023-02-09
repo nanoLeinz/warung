90206c952c7f348f52a4715390f772067e55999f
Awal Kautsar <Awallkautsar@gmail.com>
2023-02-10 01:45:38 +0700

    added UsersDto

--- src/main/java/id/kawahedu/controller/UserController.java
+++ src/main/java/id/kawahedu/controller/UserController.java
@@ -2,8 +2,10 @@ package id.kawahedu.controller;
 
 import id.kawahedu.model.UserListDto;
 import id.kawahedu.model.Users;
+import id.kawahedu.model.UsersDto;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.quarkus.hibernate.orm.panache.PanacheQuery;
+import io.quarkus.panache.common.Page;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -22,10 +24,19 @@ public class UserController {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public List<Users> getList() {
-        PanacheQuery<Users> query = Users.find();
+    public List<Users>  getList() {
+        PanacheQuery<Users> query = Users.find("status = 1");
         query.range(0, 24);
         List<Users> firstRange = query.list();
+        return firstRange;
+    }
 
+    @GET
+    @Path("/1")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<UsersDto>  getList1() {
+        PanacheQuery<UsersDto> result = Users.find("SELECT a.user_id, a.name, a.place_bd, a.email, a.role, a.status FROM Users a where status = 1").project(UsersDto.class);
+        result.page(Page.of(0,1000));
+        return result.list();
     }
 }
